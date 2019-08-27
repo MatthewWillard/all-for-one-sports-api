8ed4521a380157151bdfa1b464059297af802a7b
Matthew Willard <MWillard85@gmail.com>
2019-08-27 14:47:27 -0600

    added files, deployed heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 const app = express()
 
@@ -14,6 +21,13 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use('/api/login', loginRouter)
+app.use('/api/register', registerRouter)
+app.use('/api/NFLNews', nflNewsRouter)
+app.use('/api/NHLNews', nhlNewsRouter)
+app.use('/api/ESPN', espnRouter)
+app.use('/api/FoxSports', foxSportsRouter)
+
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
-const app = require('./app')
+const express = require('express');
+const app = express();
 
-const { PORT } = require('./config')
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};

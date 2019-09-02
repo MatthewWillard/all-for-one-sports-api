d9b62ed953e7499a479ad539a458e4da4f7baf2d
Matthew Willard <MWillard85@gmail.com>
2019-09-02 12:55:46 -0600

    update test db url

--- test/setup.js
+++ test/setup.js
@@ -1,12 +1,12 @@
 process.env.TZ = 'UTC'
 process.env.NODE_ENV = 'test'
 process.env.JWT_SECRET = 'test-jwt-secret'
-process.env.JWT_EXPIRY = '3m'
+process.env.JWT_EXPIRY = '5h'
 
 require('dotenv').config()
 
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://postgres@localhost/sports-auth"
+  || "postgresql://postgres@localhost/sports-users"
 
 const { expect } = require('chai')
 const supertest = require('supertest')
